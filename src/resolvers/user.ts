502b1018f6a3c42fa8cf1068b73f4cb5161a0d3d
Marcelo <marcelo.teixeira@code.berlin>
2022-04-13 20:32:53 +0200

    Fix nested query for books borrowed by user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -3,6 +3,7 @@ import { Item } from "../definitions/mongoose";
 const toDoc = (i: any) => i?._doc;
 
 const children = async (parent: any) => {
-  return (await Item.find({ _id: { $in: parent.children } })).map(toDoc);
+  return (await Item.find({ "rentable.currentOwner": parent._id })).map(toDoc);
 };
+
 export default { children };
